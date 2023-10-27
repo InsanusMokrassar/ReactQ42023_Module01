53141ee4eb5780eff5f404c120d4d794011bbf03
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-10-27 23:33:24 +0600

    feat: complete recent searches history

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,38 @@ import { useState } from 'react';
 import './App.css';
 import Search from './components/Search';
 import SearchPanel from './components/SearchPanel';
+import ResentSearches from './components/RecentSearches';
+import { defaultSearchHistoryWrapper } from './utils/SearchHistoryWrapper';
 
 function App() {
   const [searchState, searchStateChange] = useState('');
-  // const [currentResults, onSetResults] = useState<Array<SearchResult>>([]);
+  const [recentSearches, onSetResentSearches] = useState<Array<string>>(
+    defaultSearchHistoryWrapper.getHistory()
+  );
+
+  function updateRecentSearches() {
+    onSetResentSearches(defaultSearchHistoryWrapper.getHistory());
+  }
+
+  function doSearch(query: string) {
+    defaultSearchHistoryWrapper.add(query);
+    updateRecentSearches();
+  }
 
   return (
     <>
-      <SearchPanel onSubmit={() => window.alert(searchState)}>
-        <Search state={searchState} onChange={searchStateChange} />
-      </SearchPanel>
+      <div className="main_container">
+        <div></div>
+        <div className="main_content">
+          <SearchPanel onSubmit={() => doSearch(searchState)}>
+            <Search state={searchState} onChange={searchStateChange} />
+          </SearchPanel>
+        </div>
+        <ResentSearches
+          state={recentSearches}
+          onSelect={(query) => searchStateChange(query)}
+        />
+      </div>
     </>
   );
 }
--- src/utils/SearchHistoryWrapper.ts
+++ src/utils/SearchHistoryWrapper.ts
@@ -1,39 +1,40 @@
-// class SearchHistoryWrapper {
-//   public static searchHistoryKey: string = 'search_history';
-//   public static defaultInstance: SearchHistoryWrapper =
-//     new SearchHistoryWrapper(window.localStorage);
-//   private storage: Storage;
-//   private cache: Array<string> = [];
-//
-//   private reinitCache() {
-//     this.cache = JSON.parse(
-//       this.storage.getItem(SearchHistoryWrapper.searchHistoryKey) || '[]'
-//     );
-//   }
-//
-//   private pasteCache() {
-//     this.storage.setItem(
-//       SearchHistoryWrapper.searchHistoryKey,
-//       JSON.stringify(this.cache)
-//     );
-//   }
-//
-//   constructor(storage: Storage) {
-//     this.storage = storage;
-//     this.reinitCache();
-//   }
-//
-//   add(query: string): boolean {
-//     if (this.cache.includes(query)) {
-//       return false;
-//     } else {
-//       this.cache.push(query);
-//       this.pasteCache();
-//     }
-//     return true;
-//   }
-//
-//   getHistory(): Array<string> {
-//     return [...this.cache];
-//   }
-// }
+export default class SearchHistoryWrapper {
+  public static searchHistoryKey: string = 'search_history';
+  private storage: Storage;
+  private cache: Array<string> = [];
+
+  private reinitCache() {
+    this.cache = JSON.parse(
+      this.storage.getItem(SearchHistoryWrapper.searchHistoryKey) || '[]'
+    );
+  }
+
+  private pasteCache() {
+    this.storage.setItem(
+      SearchHistoryWrapper.searchHistoryKey,
+      JSON.stringify(this.cache)
+    );
+  }
+
+  constructor(storage: Storage) {
+    this.storage = storage;
+    this.reinitCache();
+  }
+
+  add(query: string): boolean {
+    if (this.cache.includes(query)) {
+      return false;
+    } else {
+      this.cache.push(query);
+      this.pasteCache();
+    }
+    return true;
+  }
+
+  getHistory(): Array<string> {
+    return [...this.cache];
+  }
+}
+
+export const defaultSearchHistoryWrapper: SearchHistoryWrapper =
+  new SearchHistoryWrapper(window.localStorage);
