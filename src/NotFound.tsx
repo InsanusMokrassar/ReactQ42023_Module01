dcf3e2c12fb9df25124cc00022f26ceb2a203373
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-11-11 20:09:37 +0600

    feat: add NotFound test

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -2,9 +2,12 @@ import { ReactNode } from 'react';
 
 export default function NotFound(): ReactNode {
   return (
-    <div>
+    <div role={'NotFoundContainer'}>
       Sorry, but this page do not exists in the app. Please, follow to{' '}
-      <a href={'/'}>this</a> link to continue usage of site
+      <a role={'NotFoundLink'} href={'/'}>
+        this
+      </a>{' '}
+      link to continue usage of site
     </div>
   );
 }
