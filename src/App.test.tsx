1046db9baf0e7b181f07ea5e580a56f6997ed2fa
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-11-12 16:08:08 +0600

    feat tests for Check that a loading indicator is displayed while fetching data; and Ensure that clicking the close button hides the component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -18,14 +18,22 @@ import { GithubRepository } from './utils/api/GithubApi';
 describe('App and all related tests', () => {
   const fetchMocker = createFetchMock(vi);
   let latestRequestedRepo: GithubRepository | undefined = undefined;
-
-  beforeAll(() => {
-    fetchMocker.enableMocks();
+  function enableDefaultFetchMocker(
+    onSearch: (request: Request) => string | undefined = () => undefined,
+    onRepos: (request: Request) => string | undefined = () => undefined
+  ) {
     fetchMocker.mockResponse((request) => {
       switch (true) {
         case /search/.test(request.url):
-          return JSON.stringify(testGithubResponseWithGithubRepositories);
+          return (
+            onSearch(request) ||
+            JSON.stringify(testGithubResponseWithGithubRepositories)
+          );
         case /repos/.test(request.url):
+          const onReposResult = onRepos(request);
+          if (onReposResult !== undefined) {
+            return onReposResult;
+          }
           const usernameRepoString = request.url
             .substring(request.url.indexOf('repos/') + 'repos/'.length)
             .split('/') as [string, string];
@@ -47,9 +55,14 @@ describe('App and all related tests', () => {
       }
       return '{}';
     });
+  }
+
+  beforeAll(() => {
+    fetchMocker.enableMocks();
   });
 
   it('Validate that clicking on a card opens a detailed card component;', async () => {
+    enableDefaultFetchMocker();
     const memoryProvider = createMemoryRouter(routerConfig, {
       initialEntries: ['/'],
     });
@@ -80,6 +93,7 @@ describe('App and all related tests', () => {
   });
 
   it('Check that clicking triggers an additional API call to fetch detailed information.', async () => {
+    enableDefaultFetchMocker();
     const memoryProvider = createMemoryRouter(routerConfig, {
       initialEntries: ['/'],
     });
@@ -102,8 +116,79 @@ describe('App and all related tests', () => {
     }
   });
 
+  it('Check that a loading indicator is displayed while fetching data;', async () => {
+    let checked = false;
+    enableDefaultFetchMocker(undefined, () => {
+      expect(
+        screen.getByRole('github_repository_details_loader_loading')
+      ).toBeTruthy();
+      checked = true;
+      return undefined;
+    });
+    const memoryProvider = createMemoryRouter(routerConfig, {
+      initialEntries: ['/'],
+    });
+
+    render(<RouterProvider router={memoryProvider} />);
+
+    for (
+      let i = 0;
+      i < testGithubResponseWithGithubRepositories.items.length;
+      i++
+    ) {
+      const repo = testGithubResponseWithGithubRepositories.items[i];
+      const element = await screen.findByRole(
+        `github_repository_result_container${repo.url}`
+      );
+
+      await userEvent.click(element);
+
+      expect(checked).toBeTruthy();
+      checked = false;
+    }
+  });
+
+  it('Ensure that clicking the close button hides the component.', async () => {
+    enableDefaultFetchMocker();
+    const memoryProvider = createMemoryRouter(routerConfig, {
+      initialEntries: ['/'],
+    });
+
+    render(<RouterProvider router={memoryProvider} />);
+
+    for (
+      let i = 0;
+      i < testGithubResponseWithGithubRepositories.items.length;
+      i++
+    ) {
+      const repo = testGithubResponseWithGithubRepositories.items[i];
+      const element = await screen.findByRole(
+        `github_repository_result_container${repo.url}`
+      );
+
+      await userEvent.click(element);
+
+      const closeElement = await screen.findByRole(
+        'github_repository_details_loader_close'
+      );
+      await userEvent.click(closeElement);
+
+      let found = false;
+      try {
+        screen.getByRole(
+          `github_repository_details_loader/${repo.owner.login}/${repo.name}`
+        );
+        found = true;
+      } catch (e) {
+        found = false;
+      }
+      expect(found).toBeFalsy();
+    }
+  });
+
   afterEach(() => {
     cleanup();
+    fetchMocker.mockClear();
   });
   afterAll(() => {
     fetchMocker.dontMock();
